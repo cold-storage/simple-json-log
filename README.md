cc11cb5f32a2929797590feedd3d9bcdbaf73ca8
John Stein <john.stein@fireeye.com>
2019-06-15 05:34:22 -0400

    0.0.6

--- README.md
+++ README.md
@@ -1,12 +1,88 @@
 # simple-json-log
 
-Super simple JSON logger for Node.js
+A simple JSON logger for Node.js.
+
+Light weight. No external dependencies. Great for application code or
+libraries.
+
+To use with a library, set the level based on an environment variable.
+
+```js
+const level = process.env.AWESOME_LIBRARY_LOG_LEVEL || 'off'
+const log = new(require('simple-json-log'))({ level })
+```
+
+## Quick Start
 
 ```js
 const log = new(require('simple-json-log'))({ level: 'info' })
 log.info('Hello log world!')
 ```
 
+Output
+
 ```json
-{"level":"info","time":"2019-06-14T16:32:46.237Z","info":"Hello log world!"}
+{"time":"2019-06-15T08:57:41.016Z","info":"Hello log world!"}
 ```
+
+We make it easy to log a string message and/or a JSON object.
+The string message always comes first.
+
+```js
+log.info('Hello log world!')
+log.info('Hello log world!', { some: 'nice JSON here' })
+log.info({ some: 'nice JSON here' })
+```
+
+Output
+
+```json
+{"time":"2019-06-15T09:02:20.144Z","info":"Hello log world!"}
+{"time":"2019-06-15T09:02:20.144Z","info":"Hello log world!","some":"nice JSON here"}
+{"time":"2019-06-15T09:02:20.144Z","some":"nice JSON here"}
+```
+
+## Options
+
+**`out`**
+
+Defaults to `process.stdout`. You can use any output stream you
+like.
+
+**`level`**
+
+Defaults to `off`. We do nothing unless you specify a level.
+
+**`levelAsLabel` and `label`**
+
+`levelAsLabel` defaults to `true`.
+`label` defaults to 'message'.
+
+If `levelAsLabel` is `true`, we use `level` as the message label.
+Otherwise we use `label` as the message label.
+
+**`levelElement`**
+
+Defaults to `false`. If `true` we add a `level` element to the JSON output.
+
+**`time`**
+
+Defaults to a function that outputs `new Date().toISOString()`.
+If false, we don't add a `time` element to the JSON output.
+You can also use your own function to format time however you like.
+
+**`indent`**
+
+Defaults to 0. This is the number of spaces to indent the JSON output.
+
+**`levels`**
+
+Defaults to the following. You can specify any levels you like.
+
+* trace:
+* debug:
+* info:
+* warn:
+* error:
+* fatal:
+* off:
--- index.js
+++ index.js
@@ -1,113 +1,85 @@
 #!/usr/bin/env node
 
 'use strict'
-const moment = require('moment')
 const isObject = (a) =>
   !!a && a.constructor === Object
-const levels = {
-  trace: 0,
-  debug: 1,
-  info: 2,
-  warn: 3,
-  error: 4,
-  off: 5
-}
 class Logger {
   constructor(options) {
     options = options || {}
-    this.level = levels[options.level || 'off']
-    // skipLevel: don't display the log level in output
-    this.skipLevel = options.skipLevel
-    // skipTime: don't display the log time in output
-    this.skipTime = options.skipTime
-    // messageLabel: use messageLabel as default label (instead of log level)
-    this.messageLabel = options.messageLabel
-    // indent: a number to use as indent level. See JSON.stringify(o, null, indent)
-    this.indent = options.indent
-    // replaceMessageLabelWithLevel: only matters if you pass JSON.
-    // gives you consistency between just passing a bare string and
-    // passing object. you can have both use log level as message
-    // label.
-    this.replaceMessageLabelWithLevel = options.replaceMessageLabelWithLevel
-  }
-  shouldLog(level) {
-    return levels[level] >= this.level
-  }
-  log(o, level) {
-    level = level || 'debug'
-    const messageLabel = this.messageLabel || level
-    if (!isObject(o)) {
-      o = {
-        level,
-        time: moment().toISOString(),
-        [messageLabel]: o
+    this.out = process.stdout
+    this.level = 'off'
+    this.label = 'message'
+    this.levelAsLabel = true
+    this.levelElement = false
+    this.time = () => {
+      return new Date().toISOString()
+    }
+    this.indent = 0
+    this.levels = {
+      trace: 0,
+      debug: 1,
+      info: 2,
+      warn: 3,
+      error: 4,
+      fatal: 5,
+      off: 6
+    }
+    Object.assign(this, options)
+    for (let level of Object.keys(this.levels)) {
+      this[level] = (message, json) => {
+        if (this.levels[level] >= this.levels[this.level]) {
+          let options = {}
+          if (this.time) {
+            options.time = this.time()
+          }
+          if (this.levelElement) {
+            options.level = level
+          }
+          const lbl = this.levelAsLabel ? level : this.label
+          if (json) {
+            options[lbl] = message
+            options = Object.assign(options, json)
+          } else if (isObject(message)) {
+            options = Object.assign(options, message)
+          } else {
+            options[lbl] = message
+          }
+          this.out.write(`${JSON.stringify(options, null, this.indent)}\n`)
+        }
       }
-    } else {
-      o = Object.assign({
-        level,
-        time: moment().toISOString()
-      }, o)
-    }
-    if (this.messageLabel && this.replaceMessageLabelWithLevel && o[messageLabel]) {
-      o = Object.assign({
-        level: o.level,
-        time: o.time,
-        [level]: o[messageLabel]
-      }, o)
-      delete o[messageLabel]
-    }
-    if (this.skipLevel) {
-      delete o.level
-    }
-    if (this.skipTime) {
-      delete o.time
-    }
-    console.log(JSON.stringify(o, null, this.indent))
-  }
-  trace(o) {
-    if (this.shouldLog('trace')) {
-      this.log(o, 'trace')
-    }
-  }
-  debug(o) {
-    if (this.shouldLog('debug')) {
-      this.log(o, 'debug')
-    }
-  }
-  info(o) {
-    if (this.shouldLog('info')) {
-      this.log(o, 'info')
-    }
-  }
-  warn(o) {
-    if (this.shouldLog('warn')) {
-      this.log(o, 'warn')
-    }
-  }
-  error(o) {
-    if (this.shouldLog('error')) {
-      this.log(o, 'error')
     }
   }
 }
 exports = module.exports = Logger
 if (require.main === module) {
+  const fs = require('fs')
   const log = new Logger({
-    level: 'info',
-    messageLabel: 'message',
-    replaceMessageLabelWithLevel: true,
-    skipLevel: true,
-    skipTime: true,
-    indent: 2,
+    // out: fs.createWriteStream('./logfile'),
+    level: 'trace',
+    // label: 'niceMessage',
+    // levelAsLabel: false,
+    // levelElement: true,
+    // time: false,
+    // indent: 2,
+    // levels: {
+    //   bat: 0,
+    //   zoo: 1,
+    //   monkey: 2
+    // }
   })
-  log.trace('hello trace')
-  log.debug('hello debug')
-  log.info('hello info')
-  log.warn({
-    message: 'Yo! Warning!!!',
-    obj: {
-      some: 'object here'
-    }
+  log.trace('You probably won\'t ever use this.')
+  log.debug('This is quite a bit of detail.')
+  log.info('Hello log world!')
+  log.info('Hello log world!', {
+    some: 'nice JSON here'
   })
-  log.error('hello error')
+  log.info({
+    some: 'nice JSON here'
+  })
+  log.warn('Geting low on memory.')
+  log.error('Could not get the eggs.', {
+    message: 'oops',
+    code: 39
+  })
+  log.fatal('We crashed!')
 }
